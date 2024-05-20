f8755bbedbf6e359e551137767bb9a9ebeeed3d5
Summer-lights <1067088037@qq.com>
2024-05-20 19:37:38 +0800

    feat: Fonts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Noto Sans SC, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+}
+</style>
+
