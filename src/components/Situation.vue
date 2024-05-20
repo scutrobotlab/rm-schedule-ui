272e1a5fc0eaca398ec21f627f412a3ec5dda24e
Summer-lights <1067088037@qq.com>
2024-05-20 19:29:20 +0800

    feat: UI

--- src/components/Situation.vue
+++ src/components/Situation.vue
@@ -3,61 +3,67 @@ const selectedZone = ref([0])
 </script>
 
 <template>
-  <v-img
-    class="background-image"
-    src="@/assets/background2.jpg"
-  >
-  </v-img>
+  <div>
+    <img
+      class="background-image"
+      src="@/assets/background2.jpg"
+      alt=""/>
 
-  <div class="container">
-    <v-container class="content mt-2">
-      <v-row>
-        <v-col cols="12">
-          <div>
-            <v-carousel
-              height="100vh - 100px"
-              :disabled="true"
-              hide-delimiters
-              :show-arrows="false"
-              v-model="selectedZone"
-            >
-              <v-carousel-item>
-                <SwissWheel zone="A"></SwissWheel>
-              </v-carousel-item>
-              <v-carousel-item>
-                <SwissWheel zone="B"></SwissWheel>
-              </v-carousel-item>
+    <v-img
+      class="logo"
+      src="@/assets/logo.png">
+    </v-img>
 
-            </v-carousel>
-            <v-sheet
-              class="mx-auto text-center"
-            >
-              <v-slide-group
+    <div class="container">
+      <v-container class="content mt-2">
+        <v-row>
+          <v-col cols="12">
+            <div>
+              <v-carousel
+                height="100vh - 100px"
+                :disabled="true"
+                hide-delimiters
+                :show-arrows="false"
                 v-model="selectedZone"
-                mandatory="force"
               >
-                <v-slide-group-item
-                  v-for="n in ['A', 'B']"
-                  :key="n"
-                  v-slot="{ isSelected, toggle }"
+                <v-carousel-item>
+                  <SwissWheel zone="A"></SwissWheel>
+                </v-carousel-item>
+                <v-carousel-item>
+                  <SwissWheel zone="B"></SwissWheel>
+                </v-carousel-item>
+
+              </v-carousel>
+              <v-sheet
+                class="mx-auto text-center"
+              >
+                <v-slide-group
+                  v-model="selectedZone"
+                  mandatory="force"
                 >
-                  <v-btn
-                    :color="isSelected ? 'primary' : undefined"
-                    class="ma-2"
-                    rounded
-                    @click="toggle">
-                    半区 {{ n }}
-                  </v-btn>
-                </v-slide-group-item>
-              </v-slide-group>
-            </v-sheet>
-          </div>
-        </v-col>
-        <v-img
-          src="@/assets/qr-code.png">
-        </v-img>
-      </v-row>
-    </v-container>
+                  <v-slide-group-item
+                    v-for="n in ['A', 'B']"
+                    :key="n"
+                    v-slot="{ isSelected, toggle }"
+                  >
+                    <v-btn
+                      :color="isSelected ? 'primary' : undefined"
+                      class="ma-2"
+                      rounded
+                      @click="toggle">
+                      半区 {{ n }}
+                    </v-btn>
+                  </v-slide-group-item>
+                </v-slide-group>
+              </v-sheet>
+            </div>
+          </v-col>
+          <v-img
+            src="@/assets/qr-code.png">
+          </v-img>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -81,10 +87,20 @@ const selectedZone = ref([0])
   z-index: 1;
 }
 
+.logo {
+  position: absolute;
+  bottom: 50px;
+  right: 0;
+  width: 8%;
+  opacity: 0.5;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  z-index: 3;
+}
+
 .content {
   position: relative;
   z-index: 2;
-  color: white; /* 设置内容颜色，以便在图片上可见 */
-  padding: 20px;
 }
 </style>
