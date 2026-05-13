8a4988f8bc5886a776e23fd43e705e17b5340071
Summer-lights <1067088037@qq.com>
2026-05-13 21:48:51 +0800

    feat: add new CDN URL replacement for hz-rm-bbs-web-prod to StaticCDN function

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -2,6 +2,7 @@ export function StaticCDN(url: string): string {
   url = url.replace("https://rm-static.djicdn.com", "/api/static/rm-static_djicdn_com")
   url = url.replace("https://terra-cn-oss-cdn-public-pro.oss-cn-hangzhou.aliyuncs.com", "/api/static/terra-cn-oss-cdn-public-pro_oss-cn-hangzhou_aliyuncs_com")
   url = url.replace("https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com", "/api/static/pro-robomasters-hz-n5i3_oss-cn-hangzhou_aliyuncs_com")
+  url = url.replace("https://hz-rm-bbs-web-prod.oss-cn-hangzhou.aliyuncs.com", "/api/static/hz-rm-bbs-web-prod_oss-cn-hangzhou_aliyuncs_com")
   url = url.replace("https://terra-us-pro-rm-prod-pub-us.s3.amazonaws.com", "/api/static/terra-us-pro-rm-prod-pub-us_s3_amazonaws_com")
   return url
 }
