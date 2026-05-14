0facd6a9be40f20fdeaa88cc2037782d56e49ebb
Summer-lights <1067088037@qq.com>
2026-05-15 02:37:31 +0800

    fix: update defaultGroup value for 南部赛区 in ZoneMap to ensure correct zone assignment

--- src/constant/zone.ts
+++ src/constant/zone.ts
@@ -472,7 +472,7 @@ export const ZoneMap: { [key: number]: Zone[] } = {
   2026: [
     {
       id: 614, name: '南部赛区', disabled: false,
-      defaultGroup: 0,
+      defaultGroup: 2,
       parts: [
         {
           name: 'A组前段',
