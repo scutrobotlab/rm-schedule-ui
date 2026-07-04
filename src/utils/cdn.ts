a5ed7c129c7080faf8b60c4bed38da18c4ff99a7
Summer-lights <1067088037@qq.com>
2026-07-04 13:23:09 +0800

    feat: add new CDN URL mapping for Shenzhen OSS in StaticCDN function

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -4,5 +4,6 @@ export function StaticCDN(url: string): string {
   url = url.replace("https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com", "/api/static/pro-robomasters-hz-n5i3_oss-cn-hangzhou_aliyuncs_com")
   url = url.replace("https://hz-rm-bbs-web-prod.oss-cn-hangzhou.aliyuncs.com", "/api/static/hz-rm-bbs-web-prod_oss-cn-hangzhou_aliyuncs_com")
   url = url.replace("https://terra-us-pro-rm-prod-pub-us.s3.amazonaws.com", "/api/static/terra-us-pro-rm-prod-pub-us_s3_amazonaws_com")
+  url = url.replace("https://sz-rm-rmua-dispatch-prod.oss-cn-shenzhen.aliyuncs.com", "/api/static/sz-rm-rmua-dispatch-prod_oss-cn-shenzhen_aliyuncs_com")
   return url
 }
